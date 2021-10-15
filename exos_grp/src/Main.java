436ec5145ae2919d84325a7a17e87f1642abd792
Adrien Tasic <tasticad@gmail.com>
2021-10-15 13:39:32 +0200

    Initial commit

---
+++ exos_grp/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    // User va posséder un compte en banque, et une voiture, et nom
+    // Voiture va posseder (la qtt de carburant actuelle, la qtt max de carburant, le type de carburant, nom)
+    // classes.Station va posséder un nom, une qtt de carburant, et une qtt max de carburant (Bonus associer au type de carburant)
+
+    // Menu Ajouter & Retirer de l'argent du compte
+    // Menu Regarder si il doit faire le pleins de sa voiture
+    // Menu regarder si il a les finances pour faire le pleins
+    // Menu Faire le pleins (Transaction accepté ou pas, Affichage du réservoir de sa voiture)
+
+    // TODO TypeCarburant -> Enum
+    // TODO Objet voiture(TypeCarburant) (interface ?)
+    // TODO objet classes.Station
+    // TODO objet BankAccount
+    // TODO objet User(voiture, BankAccount) va se servir d'une station pour son plein
+
+
+
+}
