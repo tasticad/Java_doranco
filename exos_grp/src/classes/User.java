1834a63fcfbadcece2e6de492d4e38ad699a5552
NonoM33 <renaudlemagicien@gmail.com>
2021-10-15 14:04:26 +0200

    Update User.java
    add fichier user

--- exos_grp/src/classes/User.java
+++ exos_grp/src/classes/User.java
@@ -1,5 +1,12 @@
-package classes;
-
 public class User {
+    String nom;
+    BankAccount bankAccount;
+    Voiture voiture;
+    public User(String nom, BankAccount bankAccount, Voiture voiture)
+    {
+        this.nom = nom;
+        this.bankAccount = bankAccount;
+        this.voiture = voiture;
+    }
 
 }
