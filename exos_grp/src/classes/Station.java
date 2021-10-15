ecd89ab5b1e48987c3f8a1faa1a56413b8152a0e
NonoM33 <renaudlemagicien@gmail.com>
2021-10-15 14:06:00 +0200

    Update Station.java
    add fichier

--- exos_grp/src/classes/Station.java
+++ exos_grp/src/classes/Station.java
@@ -1,7 +1,11 @@
-package classes;
-
 public class Station {
+    String name;
+    int qtCarburant;
+    int qtCarburantMax;
 
-
-
+    public Station(String name, int qtCarburant, int qtCarburantMax) {
+        this.name = name;
+        this.qtCarburant = qtCarburant;
+        this.qtCarburantMax = qtCarburantMax;
+    }
 }
