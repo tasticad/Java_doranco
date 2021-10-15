1e1b9c9b988eb761adbb8205bedee12a73c2c289
NonoM33 <renaudlemagicien@gmail.com>
2021-10-15 14:03:59 +0200

    Update BankAccount.java
    add fichier

--- exos_grp/src/classes/BankAccount.java
+++ exos_grp/src/classes/BankAccount.java
@@ -1,5 +1,9 @@
-package classes;
-
 public class BankAccount {
-
+    String name;
+    int solde;
+    public BankAccount(String name, int solde)
+    {
+        this.name = name;
+        this.solde = solde;
+    }
 }
