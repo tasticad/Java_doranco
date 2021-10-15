06f80855bb5183bd9c22bf47c428a8409c7903fc
NonoM33 <renaudlemagicien@gmail.com>
2021-10-15 15:00:29 +0200

    Create Main.java

---
+++ exos_grp/src/classes/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        MOI moi = new MOI();
+        Menu menu = new Menu();
+        menu.menuPrincipal(moi.user);
+    }
+}
