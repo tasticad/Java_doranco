a693a84722a1110ee9d10032a6fd3f9079a00831
NonoM33 <renaudlemagicien@gmail.com>
2021-10-15 15:00:53 +0200

    Update User.java

--- exos_grp/src/classes/User.java
+++ exos_grp/src/classes/User.java
@@ -1,7 +1,8 @@
 public class User {
-    String nom;
-    BankAccount bankAccount;
-    Voiture voiture;
+    private String nom;
+    private BankAccount bankAccount;
+    private Voiture voiture;
+
     public User(String nom, BankAccount bankAccount, Voiture voiture)
     {
         this.nom = nom;
@@ -9,4 +10,27 @@ public class User {
         this.voiture = voiture;
     }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public void setVoiture(Voiture voiture) {
+        this.voiture = voiture;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public BankAccount getBankAccount() {
+        return bankAccount;
+    }
+
+    public Voiture getVoiture() {
+        return voiture;
+    }
 }
