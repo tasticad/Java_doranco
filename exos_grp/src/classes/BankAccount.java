13ac2187f0a278c38e0228f443faf74e95260af3
NonoM33 <renaudlemagicien@gmail.com>
2021-10-15 15:02:32 +0200

    Update BankAccount.java

--- exos_grp/src/classes/BankAccount.java
+++ exos_grp/src/classes/BankAccount.java
@@ -1,9 +1,35 @@
 public class BankAccount {
     String name;
     int solde;
+
     public BankAccount(String name, int solde)
     {
         this.name = name;
         this.solde = solde;
+    }
+
+    public void addMontant(int montant)
+    {
+        this.solde += montant;
+    }
+    public void deleteMontant(int montant)
+    {
+        this.solde -= montant;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSolde() {
+        return solde;
     }
 }
